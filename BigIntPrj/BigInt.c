d2e16a806a79c9af135339fe49d3831845a23e1e
Thiago <thiago.adams@gmail.com>
2018-12-15 16:16:57 -0200

    Div (not ready)

--- BigIntPrj/BigInt.c
+++ BigIntPrj/BigInt.c
@@ -104,6 +104,87 @@ int Multiply(Digit* a, Digit aSig, Digit* b, Digit bSig, Digit* result)
 }
 
 
+int Compare(Digit* a, Digit aSig, Digit* b, Digit bSig)
+{
+    size_t i = aSig > bSig ? aSig : bSig;
+    for (;;)
+    {
+        Digit ai = (i < a) ? a[i] : 0;
+        Digit bi = (i < b) ? b[i] : 0;
+
+        if (ai < bi)
+        {
+            return -1;
+        }
+        else if (ai > bi)
+        {
+            return  1;
+        }
+
+        if (i == 0)
+        {
+            break;
+        }
+
+        i--;
+    }
+
+    return 0;
+}
+Digit TrialDigit(Digit* r,
+    Digit* d,
+    size_t k,
+    size_t m)
+{
+    // r{3} = r[k + m .. k + m - 2]
+    // d{2} = d[m - 1..m-2]
+    size_t km = k + m;
+    double rkm = r[km];
+    double rkm_1 = r[km - 1];
+    double rkm_2 = r[km - 2];
+    double dm_1 = d[m - 1];
+    double dm_2 = d[m - 2];
+    double r3 = (rkm * DIGIT_BASE + rkm_1) * DIGIT_BASE + rkm_2;
+    double d2 = dm_1 * DIGIT_BASE + dm_2;
+    double d4 = r3 / d2;
+    return d4 < (double)(DIGIT_BASE - 1) ? d4 : (double)(DIGIT_BASE - 1);
+}
+
+int Divide(Digit* a, Digit aSig, Digit* b, Digit bSig, Digit* result)
+{
+    int n = aSig;
+    int m = bSig;
+    Digit  localA[10] = { 0 };
+    Digit  localB[10] = { 0 };
+    Digit  localC[10] = { 0 };
+
+    Digit ym_1 = b[bSig];//rev
+    Digit f = (Digit)  (DIGIT_BASE / (ym_1 + 1));
+    
+    Multiply(a, aSig, &f, 1, localA);
+    Multiply(b, aSig, &f, 1, localB);
+
+
+    for (int k = n - m; k >= 0; k--)
+    {
+        Digit qt = TrialDigit(localA, localB, (size_t)k, (size_t)m);
+
+        Multiply(localB, bSig, &qt, 1, localC);
+
+        if (Compare(localA + k, k + m, localC,  m) == -1)
+        {
+            qt = qt - 1;
+            Multiply(localB, bSig, &qt, 1, localC);
+        }
+
+        result[k] = qt;
+        Subtract(localA + k, m, localC,  m, localA);
+    }
+
+    
+    return 0;
+}
+
 int Parse(const char* psz, Digit* result, int nDigits)
 {
     int localdigits = 0;
